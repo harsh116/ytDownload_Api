d12069f46b9930f4b82b140b7e4884cda8ccf689
alex <you@example.com>
2024-12-28 14:38:01 +0530

    changing remote to local

--- others/constants.js
+++ others/constants.js
@@ -1,3 +1,3 @@
-// const videoLinkHOST = "http://localhost:8080";
-const videoLinkHOST = "https://y2downloadspace-1-v8597542.deta.app";
+const videoLinkHOST = "http://localhost:8080";
+// const videoLinkHOST = "https://y2downloadspace-1-v8597542.deta.app";
 module.exports = { videoLinkHOST };
